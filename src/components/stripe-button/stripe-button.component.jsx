a00b7511dba2fc001a750b8bb758278979381ac1
ZeeshanAmjad0495 <zeeshan04956@gmail.com>
2021-02-18 21:21:15 +0500

    checkout, payment, shop, categories implemented

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51IMDQ2I87ohsNxqIJpimZ6c9JmIklQ4sOQW0sYKutU7gFMHtSbxMceS0hOXgK1QqJaU7j2b7zUPePHJif8bt4HIY00ZfDGPgJb";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
